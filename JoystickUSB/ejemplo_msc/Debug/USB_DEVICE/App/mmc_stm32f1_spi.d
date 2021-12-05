6e26a764605133b6182758cc0a8c58517574d092
tbavassopiizzi <tbavassopiizzi@frba.utn.edu.ar>
2021-12-05 10:12:31 -0300

    finish year

--- JoystickUSB/ejemplo_msc/Debug/USB_DEVICE/App/mmc_stm32f1_spi.d
+++
@@ -1,6 +0,0 @@
-USB_DEVICE/App/mmc_stm32f1_spi.o: ../USB_DEVICE/App/mmc_stm32f1_spi.c \
- ../USB_DEVICE/App/STM32F100.h ../USB_DEVICE/App/diskio.h
-
-../USB_DEVICE/App/STM32F100.h:
-
-../USB_DEVICE/App/diskio.h:
